db7b0b638046c0308b66f36fe79338e59d92623b
EricR <alu0100786330@ull.edu.es>
2017-03-14 18:45:55 +0000

    sweet alert

---
+++ dia2/javascript/script5.js
@@ -0,0 +1,11 @@
+function mensaje() {
+ swal({
+ title: 'Buenos días',
+ text: '¿Como está usted?',
+ imageUrl: 'http://sumedico.com/wp-content/uploads/2016/06/C%C3%B3mo_saber_si_tu_gato_te_quiere.jpg',
+ imageWidth: 440,
+ imageHeight: 250,
+ padding: 20,
+ animation: false,
+ });
+}
