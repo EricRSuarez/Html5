fca211fea893822c31d499c848c0a1fb219e01f9
EricR <alu0100786330@ull.edu.es>
2017-03-15 17:37:59 +0000

    "añadiendo 6"

--- dia3/javascript/script5.js
+++ dia3/javascript/script5.js
@@ -1,13 +1,1 @@
-$('.editor').trumbowyg({
-	 lang: 'es', // Idioma (es.min.js incluido)
-	 btns: [
-	 ['bold', 'italic', 'underline', 'strikethrough'],
-	 ['superscript', 'subscript'],
-	 ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
-	 ['unorderedList', 'orderedList'],
-	 ['link', 'insertImage'],
-	 ['horizontalRule', 'removeformat'],
-	 ['viewHTML'],
-	 ['fullscreen']
-	 ],
-});
+$('.editor').trumbowyg();
---
+++ dia3/javascript/script6.js
@@ -0,0 +1,6 @@
+var waypoint = new Waypoint({
+  element: document.getElementById('tres'),
+  handler: function(direction) {
+    console.log('Scrolled to waypoint tres!')
+  }
+});
