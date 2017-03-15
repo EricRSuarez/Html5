004ce8fe201b1cab214087625487a2115415e54f
EricR <alu0100786330@ull.edu.es>
2017-03-15 18:52:11 +0000

    7 funcionando menu guiado

--- dia3/javascript/script5.js
+++ dia3/javascript/script5.js
@@ -1,5 +1,16 @@
 $('.editor').trumbowyg({
- svgPath: 'images/icons.svg'
+ svgPath: 'images/icons.svg',
+ lang: 'es', // Idioma (es.min.js incluido)
+ btns: [
+ ['bold', 'italic', 'underline', 'strikethrough'],
+ ['superscript', 'subscript'],
+ ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+ ['unorderedList', 'orderedList'],
+ ['link', 'insertImage'],
+ ['horizontalRule', 'removeformat'],
+ ['viewHTML'],
+ ['fullscreen']
+ ]
  // svgPath: '/icons/icons.svg', // path of icons.svg
  // Habitualmente, se indica la ruta del SVG en este parámetro, pero
  // en codepen, simplemente pegaremos el contenido del SVG en la parte
