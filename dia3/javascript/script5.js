4b97213cea27edde4aabbd1096c48e0eb80b43c8
EricR <alu0100786330@ull.edu.es>
2017-03-15 17:13:57 +0000

    "añadiendo el ultimo no funciona mucho"

---
+++ dia3/javascript/script5.js
@@ -0,0 +1,13 @@
+$('.editor').trumbowyg({
+	 lang: 'es', // Idioma (es.min.js incluido)
+	 btns: [
+	 ['bold', 'italic', 'underline', 'strikethrough'],
+	 ['superscript', 'subscript'],
+	 ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+	 ['unorderedList', 'orderedList'],
+	 ['link', 'insertImage'],
+	 ['horizontalRule', 'removeformat'],
+	 ['viewHTML'],
+	 ['fullscreen']
+	 ],
+});
